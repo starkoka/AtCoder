a3630b70723f2fc8eab455321282d30ea231c165
starkoka <j22437@inc.kisarazu.ac.jp>
2023-06-30 21:57:21 +0900

    hoge

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -1,14 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using intp = pair<int,int>;
+using uset = unordered_set<int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+#define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i) //繰り返し
+#define siz(x) ((int)(x).size()) //sizeの取得のunsigned対策
+#define vsort(x) sort(x.begin(),x.end());
+#define F first
+#define S second
+#define bit(n) (1LL<<(n))
+#define nl <<endl
+#define qq <<
+#define pp >>
 
-int main(){
-    int n,k,ans=0;
-    cin >> n >> k;
-    for(int i = 1; i <= min(k-2,n); i++){
-        for(int j=1;j <= min(n,k-i-1);j++){
-            if(k-i-j<=n)ans++;
-        }
-    }
-    cout << ans << endl;
+
+int main(void){
 
 }
