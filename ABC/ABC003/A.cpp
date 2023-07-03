f32f3a42e11af131d68689a103254827992ad341
starkoka <j22437@inc.kisarazu.ac.jp>
2023-07-03 22:25:27 +0900

    Added ABC003

---
+++ ABC/ABC003/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using intp = pair<int,int>;
+using uset = unordered_set<int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+#define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i) //繰り返し
+#define siz(x) ((int)(x).size()) //sizeの取得のunsigned対策
+#define vsort(x) sort(x.begin(),x.end());
+#define F first
+#define S second
+#define bit(n) (1LL<<(n))
+#define nl "\n"
+#define qq <<
+#define pp >>
+
+int main(void){
+    int n,ans=0;
+    cin pp n;
+    rep(i,n){
+        ans += (i+1)*10000;
+    }
+    cout qq ans/n qq nl;
+}
