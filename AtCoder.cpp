816e92e572cbe305e47ab5e73270903969491776
starkoka <j22437@inc.kisarazu.ac.jp>
2023-08-08 23:39:38 +0900

    一個前のコミット、~057だったっていうね

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -17,31 +17,5 @@ using vcc = vector<vector<char>>;
 
 
 int main(){
-    int n,m;
-    cin >> n >> m;
-    vector<intp> ab(n);
-    rep(i,n){
-        int a,b;
-        cin >> a >> b;
-        ab[i]=make_pair(a,b);
-    }
 
-    vector<intp> cd(m);
-    rep(i,m){
-        int c,d;
-        cin >> c >> d;
-        cd[i]=make_pair(c,d);
-    }
-
-    rep(i,n){
-        intp ans={2000000001,0};
-        rep(j,m){
-            int d = abs(ab[i].F-cd[j].F) + abs(ab[i].S-cd[j].S);
-            if(ans.F>d){
-                ans.F=d;
-                ans.S=j+1;
-            }
-        }
-        cout << ans.S << nl;
-    }
 }
