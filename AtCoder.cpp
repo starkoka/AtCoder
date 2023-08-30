3e220b11cbf3a0b7e57f24fb54e70a02600935c6
starkoka <j22437@inc.kisarazu.ac.jp>
2023-08-30 22:26:51 +0900

    鉄則ディレクトリの整理

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -7,21 +7,32 @@ using vi = vector<int>;
 using vii = vector<vector<int>>;
 using vc = vector<char>;
 using vcc = vector<vector<char>>;
-#define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i) //繰り返し
-#define vsort(x) sort(x.begin(),x.end());
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
 #define F first
 #define S second
 #define nl "\n"
 
 
 int main() {
-    int maxP=INT_MAX*-1,n,ans=0;
+    int n;
     cin >> n;
-    rep(i,n){
-        int p;
-        cin >> p;
-        ans += p;
-        maxP = max(maxP,p);
+    vi a(n+1,INT_MAX),b(n+1,INT_MAX);
+    rep(i,2,n+1){
+        cin >> a[i];
     }
-    cout << ans-maxP/2 << nl;
+    rep(i,3,n+1){
+        cin >> b[i];
+    }
+
+    vi dp(n+1);
+    dp[1] = 0;
+    dp[2] = a[2];
+    rep(i,3,n+1){
+        dp[i] = min(dp[i-1]+a[i] , dp[i-2]+b[i]);
+    }
+
+    cout << dp[n] << nl;
 }
