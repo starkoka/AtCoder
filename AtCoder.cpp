7067859bb719a6a755b0c0b3d6ba07ffa6564444
starkoka <j22437@inc.kisarazu.ac.jp>
2023-09-07 23:58:53 +0900

    ABC131-A

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -16,5 +16,31 @@ using vcc = vector<vector<char>>;
 #define nl "\n"
 
 
-int main(){
+int main() {
+    int n,k;
+    string s;
+    cin >> n >> k >> s;
+
+    vi vec;
+    int num=0,now=(s[0]=='1'?0:1);
+    if(s[0]=='0')vec.push_back(0);
+    vec.push_back(0);
+    rep(i,0,n){
+        num++;
+        vec[now]=num;
+        if(s[num]!=s[num-1]){
+            now ++;
+            if(i!=n-1)vec.push_back(0);
+        }
+    }
+
+    int ans = INT_MAX*-1;
+    for(int left=0;left<vec.size();left+=2){
+        int right = left + 2*k;
+        if(right>=vec.size())right=vec.size()-1;
+        ans = max(ans,vec[right]-(left==0 ? 0 : vec[left-1]));
+    }
+
+    cout << ans << nl;
+    return 0;
 }
