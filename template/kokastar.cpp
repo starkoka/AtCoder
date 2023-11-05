7c1bb7ae4b021e553baf636e6bfea8f7ed00cdfb
starkoka <j22437@inc.kisarazu.ac.jp>
2023-11-05 22:40:28 +0900

    鉄則B29

--- template/kokastar.cpp
+++ template/kokastar.cpp
@@ -1,69 +1,3 @@
-#include <bits/stdc++.h>
-#include <fstream>
-using namespace std;
-using ll = long long;
-using intp = pair<int,int>;
-using uset = unordered_set<int>;
-using vi = vector<int>;
-using vii = vector<vector<int>>;
-using vc = vector<char>;
-using vcc = vector<vector<char>>;
-using minp_queue = priority_queue<intp, vector<intp>, greater<intp>>;
-#define rep(i,a,b) for(int i=a;i<b;i++)
-#define rrep(i,a,b) for(int i=a;i>=b;i--)
-#define fore(i,a) for(auto &i:a)
-#define all(x) (x).begin(),(x).end()
-#define outset(x) cout << setprecision(x)
-#define F first
-#define S second
-#define nl "\n"
-template <typename T>
-bool chmax(T &a,const T& b){if(a<b){a=b;return true;}return false;}
-template <typename T>
-bool chmin(T &a,const T& b){if(a>b){a=b;return true;}return false;}
-typedef string str;
-typedef long l;
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<str> vs;
-typedef vector<l> vl;
-typedef vector<ll> vll;
-#define fi(s, e) for(int i = (s); i < (e); i++)
-#define f0i(e) fi(0,(e))
-#define fj(s, e) for(int j = (s); j < (e); j++)
-#define f0j(e) fj(0,(e))
-#define fk(s, e) for(int k = (s); k < (e); k++)
-#define f0k(e) fk(0,(e))
-#pragma GCC target("avx2")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
-
-#ifdef LOCAL
-#  include "debug_print.hpp"
-#  define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
-#else
-#  define debug(...) (static_cast<void>(0))
-#endif
-
-void setup();
-
-int main(){
-
-
-	return 0;
-}
-
-void setup(){
-    #ifdef LOCAL
-        ifstream inputFile("input.txt");
-		cin.rdbuf(inputFile.rdbuf());
-    #else
-        cin.tie(0); ios::sync_with_stdio(0);
-    #endif
-    cout<<fixed<<setprecision(10);
-}
-
 /* I am kokastar
                                      .!!!(*(("""((!!!!!!!!!'`          ```..```
 `..`````                         `!""!!!.```    ``.'!!!'!!!!"I!'                ````````
@@ -112,3 +46,46 @@ void setup(){
                                   ..                .!"(+"!`    ````.```` `
 
  */
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+using ll = long long;
+using intp = pair<int,int>;
+using uset = unordered_set<int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using minp_queue = priority_queue<intp, vector<intp>, greater<intp>>;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+#define outset(x) cout << setprecision(x)
+#define F first
+#define S second
+#define nl "\n"
+template <typename T>
+bool chmax(T &a,const T& b){if(a<b){a=b;return true;}return false;}
+template <typename T>
+bool chmin(T &a,const T& b){if(a>b){a=b;return true;}return false;}
+#define MOD_TEN 1000000007
+#define MOD_NINE 998244353
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+void setup(){
+#ifdef LOCAL
+    ifstream inputFile("input.txt");
+		cin.rdbuf(inputFile.rdbuf());
+#else
+    cin.tie(0); ios::sync_with_stdio(0);
+#endif
+    cout<<fixed<<setprecision(10);
+}
+#ifdef LOCAL
+#  include "debug_print.hpp"
+#  define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#  define debug(...) (static_cast<void>(0))
+#endif
