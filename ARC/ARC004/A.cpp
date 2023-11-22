0c751c10bbf17147d92db7b4e7f060ff6aa6bbcf
starkoka <j22437@inc.kisarazu.ac.jp>
2023-11-23 02:34:39 +0900

    .size() はunsigned intです。すみませんでした(ARC005-A)

--- ARC/ARC004/A.cpp
+++ ARC/ARC004/A.cpp
@@ -1,4 +1,113 @@
+/* I am kokastar
+                                     .!!!(*(("""((!!!!!!!!!'`          ```..```
+`..`````                         `!""!!!.```    ``.'!!!'!!!!"I!'                ````````
+           `````                !(! ``                        !!""*!-
+                  `.````                                          `!(3*`
+              `  ` .("!!!*"' ````````                                 !(""!-
+               ` '""'````           `..``````                            `'(%!  `
+             ` `"". ` `                       ``.```````                  `` !%! ``
+              '$!`                                   ```````````          `    !3!  `
+           ` (3`            '"(`         ``  .!`               `` ```  .`        (3.
+          ` u"             `$!          `#I !"$!.$$j!                      ````  `'$' `
+```````` .$!              !$          .$$#j"``("! !$.          `                 `'#.``..
+         !$! .`..`       !!"!         .t!``       ` "I                 !.           "j
+    ` ` ""`       ```..`"$'%!         "!            '%` ` ""%'         !$'          !#`
+ ` ```!&!      `   `   (+  (3```..`` *!             `$.  .%`""          !3         ` $(
+    `"+` ``!!`        '$"(""#! `   `!"``..````      .$` `I! !$          '#         ` "%
+   !"'!!""3#!    `   `$"!!!'(#!    .u.        ````.`!$``j!  '$+*""*!! ` !%           !#`
+`!$%""!' `$!      ` `"$!!!!!*(""`  !" `           ` (".u! ` !#%"""I"#&  $!            #!
+'!!`  `  !$        ` I! ``     !u(-$!``            .3"I'    I*'... .!! ($.            $!
+         !$     `   !z` `!!(!!`  '(#'```         ` (#(.   !###$$$$#z!.!j!% ` ``.``` ` %*
+```````` `$!     !- 3!!%$%j"$##t   `              ` `     "#!(#'!!!!u#$ '$.         ``%+``
+       ```!#'```!#"'$#!   '#%3*$'  `                      !!`##$"!"I"+I`'$`   `       $!
+           !$- !$'3$#!`.. $(%!%!#'`   `                   ` .#%#$#3J$-  '$  !+       '$`
+            !$!#!`'##`   !$%*$'I#( ```       `   `` `       .$$###$($'  ("  "!      `%!
+            `-%!  .#!    .##%#"%#( ` '"`!""""u( *#$.        `#%!!!!%u   J! !$"(z! ` !3
+                   $!('   "#!` '#.   "%$.!"++*(*!$#( ` .     !%!.`!$. -!$`'""###!  !#!
+                  `%"#! `  !$("$!   !%!#.        *%"j-.  `` .`.!""!`.!##!'%.  `"I.!""
+                   j"!$3!!.` ..`'!"#$*('  '' `    !u"$$"!!!!!'''!(3#$#(&!%.`` `"" !!  `
+                   $+  `!!"%$$$%"(!` `    !!`    `` `!!!(!((((!(!!!!. +#( `` !3!`'``` `
+`````              #!  `                                         ` ``.z!  .("!        ````
+      `.```````` ` $z .`!!'  `                                    -"""""!.'- `
+                `` .$3!$$*!$'                                     !I"""!!$! ` ``
+                     !##!(I!. .`````         `-!!!!                     "$u! `
+                      `!!"u*!`       ``.``.#"(!!!!%"     `       .``!!""#(`
+                         ` '!"""!` `  ` ` `*%!!.` %+``  ``'!!"""""-`!` !z
+                               `!IJ"(!`     `'- .!!!(""""3$!!` ``...` .$.
+                                    -!"jI"*(I"3"+!!-`   !$'`!"*+""!!((3u```
+                                   . `  $(```          .$..` ``   `          .-``````
+`````                          .!!! .! !#` ` `       ` '$.!"""!!`                     ````
+   ``````````               !""!!'' !#($$"!!!'`    .'!(%%'    .!*"!. `
+              ```````.. ` `!$`     `j#!   .!!!!++j33ju"$'`  `    'I#!
+                        ````!3.````'$.(t!!` `.!!"""!!!$! ` `'!!""!!`
+                             .I!`  I(``.'!(((!!.    `3!   !3!'`
+                               !u!!$`   `    `````` (" ` .$'
+                                 !#%                $!  `'#!``` `
+                                  ..                .!"(+"!`    ````.```` `
 
-//
-// Created by kokastar on 23/11/23.
-//
+ */
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+using ll = long long;
+using intp = pair<int,int>;
+using uset = unordered_set<int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using minp_queue = priority_queue<intp, vector<intp>, greater<intp>>;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+#define outset(x) cout << setprecision(x)
+#define F first
+#define S second
+#define nl "\n"
+#define MOD_TEN (ll)1000000007
+#define MOD_NINE (ll)998244353
+template <typename T>
+bool chmax(T &a,const T& b){if(a<b){a=b;return true;}return false;}
+template <typename T>
+bool chmin(T &a,const T& b){if(a>b){a=b;return true;}return false;}
+/*
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+*/
+void setup(){
+#ifdef LOCAL
+    ifstream inputFile("input.txt");
+		cin.rdbuf(inputFile.rdbuf());
+#else
+    cin.tie(0); ios::sync_with_stdio(0);
+#endif
+    cout<<fixed<<setprecision(10);
+}
+#ifdef LOCAL
+#  include "debug_print.hpp"
+#  define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#  define debug(...) (static_cast<void>(0))
+#endif
+
+
+int main(){
+    setup();
+    int n;
+    cin >> n;
+    vector<intp> vec(n);
+    rep(i,0,n) {
+        cin >> vec[i].F >> vec[i].S;
+    }
+    int maxLong = 0;
+    rep(i,0,n-1) {
+        rep(j,i+1,n) {
+            chmax(maxLong,(vec[i].F-vec[j].F)*(vec[i].F-vec[j].F)+(vec[i].S-vec[j].S)*(vec[i].S-vec[j].S));
+        }
+    }
+
+    outset(6);
+    cout << sqrt(maxLong) << nl;
+}
