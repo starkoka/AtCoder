e364d0c72fe1d399c8cd9f87118d050a34a78691
starkoka <j22437@inc.kisarazu.ac.jp>
2024-05-26 02:02:51 +0900

    multiTest.pyをseed値指定できるように改良

--- multiTest.py
+++ multiTest.py
@@ -13,11 +13,18 @@ def find_files(root_dir, extension):
 files = find_files("./input/", "txt")
 print(str(len(files)) + "個のテストケースを見つけました。")
 
+print("初期シード値を入力：",end="")
+seed = int(input())
+
 os.system("g++ AtCoder.cpp")
 print("コードコンパイル実行完了。テストケースを実行します...")
 
 for i in range(len(files)):
-    os.system("./a.out < input/" + files[i] + "> output/" + files[i])
+    number_len = len(files[i])-len(".txt")
+    number = int(files[i][0:number_len])
+    number += seed
+    output_filename = str(number).zfill(number_len) + ".txt"
+    os.system("./a.out < input/" + files[i] + "> output/" + output_filename)
     print("\r" + str(i + 1) + "個完了 / " + str(len(files)) + "個中", end="")
 
 print("\n完了しました")
